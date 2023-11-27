3ab19e00c0b300d0c04ab23b23ed4cdc62d16812
Botao Han <73296496+124135417@users.noreply.github.com>
2023-11-27 03:30:11 -0500

    init

---
+++ datasets/images/README.dataset.txt
@@ -0,0 +1,6 @@
+# Car maintenance > 2023-11-26 12:38am
+https://universe.roboflow.com/hri-wsy49/car-maintenance
+
+Provided by a Roboflow user
+License: CC BY 4.0
+
---
+++ train.py
@@ -0,0 +1,10 @@
+from ultralytics import YOLO
+
+if __name__ == "__main__":
+    # Load a model
+    model = YOLO("yolov8n.yaml")  # build a new model from scratch
+
+    # Use the model
+
+    results = model.train(data="config.yaml", epochs=1)  # train the model
+
